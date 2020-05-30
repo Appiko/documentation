1ea3180012f503dc59df87ea41c1ecc6b72d3599
Prathik <pratik862pj@live.com>
2020-05-30 13:58:24 +0530

    embed and color

--- web-client/flutter_service_worker.js
+++ web-client/flutter_service_worker.js
@@ -7,9 +7,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "manifest.json": "5675593dbcbdeca204436f1b9571dde5",
-"main.dart.js": "0286f0c27befee683fdb86c61b92a117",
+"main.dart.js": "4a4bf41dc396022a35e5148145132447",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/LICENSE": "6bf23aa9322a4ccf856e3dfe5567d00b",
+"assets/LICENSE": "2636366446bab7c390f6e84c78bc7f37",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
