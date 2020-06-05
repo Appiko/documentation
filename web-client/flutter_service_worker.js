887e8970bf801c9e001980efe9a459d27e2cdd29
Prathik <pratik862pj@live.com>
2020-06-05 19:15:22 +0530

    uuid on page load, validate unit fields, better text

--- web-client/flutter_service_worker.js
+++ web-client/flutter_service_worker.js
@@ -7,9 +7,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "manifest.json": "5675593dbcbdeca204436f1b9571dde5",
-"main.dart.js": "72ed5a8198ab767ab06e4751c7c691dd",
+"main.dart.js": "67894ed2307b1d20ddf6ec0b94a141c4",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/LICENSE": "34d4b11adb9ba8d34518097ca87156b8",
+"assets/LICENSE": "d4f5693098348791b74964395aed9379",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
