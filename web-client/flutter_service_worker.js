2aef4857f2bb8b6d8f2330ad120b2fea8e653eb6
Prathik <pratik862pj@live.com>
2020-06-05 20:13:20 +0530

    ✨ better pdf

--- web-client/flutter_service_worker.js
+++ web-client/flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "manifest.json": "5675593dbcbdeca204436f1b9571dde5",
-"main.dart.js": "67894ed2307b1d20ddf6ec0b94a141c4",
+"main.dart.js": "91a8691dddbe802d7df15a74e708ff1d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/LICENSE": "d4f5693098348791b74964395aed9379",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
